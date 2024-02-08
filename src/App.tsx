c729e48a86549dff7f20fcb7b142758ff1ac2ce7
Justin Walsh <contact.me@thejustinwalsh.com>
2024-02-08 04:14:22 -0500

    fix: add BackgroundProvider to set the root view to the themes bg color

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,45 @@ if (__DEV__) {
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import {TamaguiProvider} from 'tamagui';
+import {TamaguiProvider, useTheme} from 'tamagui';
+
+import config from './tamagui.config';
 
 import type {StackProps} from '@app/navigation/params';
-import config from './tamagui.config';
-//import {appBridge} from './lib/native';
+
+import {appBridge} from '@app/lib/native';
 import HomeScreen from '@app/screens/HomeScreen';
 import DetailsScreen from '@app/screens/DetailsScreen';
 
 const Stack = createStackNavigator<StackProps>();
 
+function BackgroundProvider({children}: {children: React.ReactNode}) {
+  appBridge.setBackgroundColor(useTheme().background.get() || '#000000');
+  return <>{children}</>;
+}
+
 function App(): JSX.Element {
   return (
     <TamaguiProvider config={config} disableInjectCSS defaultTheme="dark">
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerShown: false,
-            animationEnabled: true,
-            cardStyleInterpolator: ({current}) => ({
-              cardStyle: {
-                opacity: current.progress,
-              },
-            }),
-          }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <BackgroundProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+              headerShown: false,
+              animationEnabled: true,
+              detachPreviousScreen: true,
+              cardStyleInterpolator: ({current}) => ({
+                cardStyle: {
+                  opacity: current.progress,
+                },
+              }),
+            }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </BackgroundProvider>
     </TamaguiProvider>
   );
 }
